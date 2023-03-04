5f3c0e589ab55f79f1a05e72f57ba7686c26035d
NisalJayathilaka <nisal7075399@gmail.com>
2023-03-04 22:17:08 +0530

    user service

---
+++ backend/user-service/src/main/java/com/nisalj/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nisalj.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
