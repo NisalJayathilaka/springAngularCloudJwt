269324325f59ed3def29d80d38d423231ae84aa7
NisalJayathilaka <nisal7075399@gmail.com>
2023-03-04 22:25:32 +0530

    user entity

---
+++ backend/user-service/src/main/java/com/nisalj/userservice/entity/UserEntity.java
@@ -0,0 +1,59 @@
+package com.nisalj.userservice.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+    @Column(name = "user_name")
+    private String userName;
+    private String password;
+    private String email;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(int id, String userName, String password, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
